f7ccf012e11458af090cb32e92ee20842a73f3af
ErikSuca <erik.sucasai@gmail.com>
2021-06-01 20:36:44 -0300

    se agrego las rutas que faltaban y ahora funciona completamente

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ app.listen(3030, ()=>{
     console.log("Servidor Activo");
 })
 
-app.use(express.static('../public'));
-const web = require('./routes/web');
-app.use(web);
+app.use(express.static('../public')); // los archivos html se referencian desde esta pagina y no es necesario ingrar a la carpeta public posteriormente. 
+const web = require('./routes/web'); 
+app.use(web); // usa las rutas desde el archivo web.js 
--- src/routes/web.js
+++ src/routes/web.js
@@ -2,32 +2,36 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 
-router.get('/home', (req,res)=>{
+router.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname, '../views/index.html'))
 });
+ 
+router.get('/babbage', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './../views/babbage.html'))
+});
 
-// router.get('/', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './../views/'))
-// });
+router.get('/berners-lee', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './../views/berners-lee.html'))
+});
 
-// router.get('/', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './../views/'))
-// });
+router.get('/clarke', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './../views/clarke.html'))
+});
 
-// router.get('/', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './../views/'))
-// });
+router.get('/hamilton', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './../views/hamilton.html'))
+});
 
-// router.get('/', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './../views/'))
-// });
+router.get('/hopper', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './../views/hopper.html'))
+});
 
-// router.get('/', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './../views/'))
-// });
+router.get('/lovelace', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './../views/lovelace.html'))
+});
 
-// router.get('/', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './../views/'))
-// });
+router.get('/turing', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './../views/turing.html'))
+});
 
 module.exports = router;
